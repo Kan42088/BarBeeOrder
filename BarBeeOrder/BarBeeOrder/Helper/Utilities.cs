1c444543f6572a0f9d1e52a6e090c031abfff103
Kan42088 <vuongpttn99@gmail.com>
2022-03-01 14:33:06 +0700

    Thêm hàm upload ảnh vào dịch vụ

--- BarBeeOrder/BarBeeOrder/Helper/Utilities.cs
+++ BarBeeOrder/BarBeeOrder/Helper/Utilities.cs
@@ -1,7 +1,85 @@
-﻿namespace BarBeeOrder.Helper
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarBeeOrder.Helper
 {
     public static class Utilities
     {
+        public static void CreateIfMissing(string path)
+        {
+            bool folderExits = Directory.Exists(path);
+            if (!folderExits)
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
 
+        public static string ToTitleCase(string str)
+        {
+            string result = str;
+            if (!string.IsNullOrEmpty(str))
+            {
+                var words = str.Split(' ');
+                for (int index = 0; index < words.Length; index++)
+                {
+                    var s = words[index];
+                    if (s.Length > 0)
+                    {
+                        words[index] = s[0].ToString().ToUpper() + s.Substring(1);
+                    }
+                    result = string.Join(" ", words);
+                }
+            }
+            return result;
+        }
+
+        public static string GetRandomKey(int length = 5)
+        {
+            string pattern = @"0123456789zxcvbnmasdfghjklqwertyuiop[]{}:~!@#$%^&*()_+";
+            Random rd = new Random();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                sb.Append(pattern[rd.Next(0, pattern.Length)]);
+            }
+            return sb.ToString();
+        }
+
+
+        public static async Task<string> UploadFile(Microsoft.AspNetCore.Http.IFormFile file, string sDirectory, string newName)
+        {
+            try
+            {
+                if (newName == null)
+                {
+                    newName = file.FileName;
+                }
+                string path= Path.Combine(Directory.GetCurrentDirectory(),"wwwroot", "images", sDirectory);
+                CreateIfMissing(path);
+                string pathFile = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", newName);
+                var supportedTypes = new[] { "jpg", "jpeg", "png", "gif" };
+                var fileExt = System.IO.Path.GetExtension(file.FileName).Substring(1);
+                if (!supportedTypes.Contains(fileExt.ToLower()))
+                {
+                    return null;
+                }
+                else
+                {
+                    using (var stream = new FileStream(pathFile,FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                    return newName;
+                }
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
     }
 }
