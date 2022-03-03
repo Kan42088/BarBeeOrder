a52361e13230c807189d5b5f14f9c5d3d2cde06a
Kan42088 <vuongpttn99@gmail.com>
2022-03-03 13:29:30 +0700

    Cập nhật model product
    thêm các trường trạng thái

--- BarBeeOrder/BarBeeOrder/Models/Product.cs
+++ BarBeeOrder/BarBeeOrder/Models/Product.cs
@@ -26,6 +26,8 @@ namespace BarBeeOrder.Models
         public string Tittle { get; set; }
         public string Thumb { get; set; }
         public bool BestSellers { get; set; }
+        public bool Active { get; set; }
+        public bool HomeFlag { get; set; }
 
         public virtual Category Category { get; set; }
         public virtual ICollection<AttributePrice> AttributePrices { get; set; }
